898c6e528ba2ee02ecce99bd53915d7475ff5ca7
Mahiat Mahin <42869069+Mahin493@users.noreply.github.com>
2022-01-19 20:35:15 +0600

    1013 The Greatest

---
+++ URI And UVA/Uri_1013_The Greatest.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+int a,b,c,maximum;
+cin>>a>>b>>c;
+maximum=a;
+
+if(b>maximum)
+   maximum=b;
+
+ if(c>maximum)
+      maximum=c;
+
+ cout<<maximum<<" eh o maior"<<endl;
+
+
+
+
+
+}
