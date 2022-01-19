b0d9a372482dd2781238c64de3135d7f78354b89
Mahiat Mahin <42869069+Mahin493@users.noreply.github.com>
2022-01-19 20:37:31 +0600

    average 2

---
+++ URI And UVA/URI_1006_Average2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main() {
+
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+
+     double A,B,C,Average;
+     cin>>A>>B>>C;
+
+     Average=(A*2+B*3+C*5)/(2+3+5);
+
+     cout<<fixed<<setprecision(1)<<"MEDIA = "<<Average<<endl;
+
+
+
+    return 0;
+}
