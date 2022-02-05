46d45367b43e6f2bab8c7f2631fb8b7bd174197b
Mahiat Mahin <42869069+Mahin493@users.noreply.github.com>
2022-02-05 21:48:52 +0600

    Adding Two numbers

---
+++ CodeChef/AddTwoNumbers.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int range,sum=0;
+ cin>>range;
+
+for(int i=0;i<range;i++){
+      int num1,num2;
+      cin>>num1>>num2;
+      sum=num1+num2;
+      cout<<sum<<endl;
+
+}
+
+
+}
